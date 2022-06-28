9fc4011131c0513355691fc35459f617975b6b95
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-06-28 03:00:21 -0800

    Feature: Black pen works on the container

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 let container = document.getElementById('container');
-let gridNumber = 64;
+let gridNumber = 16;
 let color = 'white';
 let border = 'solid black 0.01px';
-
-
+/*
+function hover(newColor = 'black'){
+    newColor = newColor.toLowerCase();
+    this.style.backgroundColor = newColor;
+}
+*/
 function createGrid(gridNumber){
     for(let i = 0; i < (gridNumber**2); i++){
         let div = document.createElement('div');
@@ -13,6 +17,14 @@ function createGrid(gridNumber){
         container.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
         container.appendChild(div);
     };
-
+    let gridItems = container.getElementsByTagName('div');
+    for(let i = 0; i < gridItems.length;i++){
+        gridItems[i].addEventListener('mouseover', function () {this.style.backgroundColor = 'black'});
+    };
+    /*
+    gridItems.forEach(gridItem => {
+        gridItem.addEventListener('mouseover', hover())
+    });
+    */
 }
 createGrid(gridNumber);
